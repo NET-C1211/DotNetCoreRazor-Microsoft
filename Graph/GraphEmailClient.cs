fd7322a63fb8ae136a7f89fb5d320d6b146d1ab3
Bernard Bolton <boltonbernard23@gmail.com>
2021-08-19 08:45:15 -0700

    Update to UserMessagesCollectionRequest (thanks Andrew Omondi)

--- Graph/GraphEmailClient.cs
+++ Graph/GraphEmailClient.cs
@@ -53,7 +53,6 @@ namespace DotNetCoreRazor_MSGraph.Graph
         {
             int top = 5;
             IUserMessagesCollectionPage pagedMessages;
-            string nextLink;
             
             if (nextPageLink == null) 
             {
@@ -68,38 +67,47 @@ namespace DotNetCoreRazor_MSGraph.Graph
                             msg.ReceivedDateTime
                         })
                         .Top(top)
-                        .OrderBy("receivedDateTime")
+                        .OrderBy("receivedDateTime desc")
                         .GetAsync();
-                nextLink = pagedMessages.AdditionalData["@odata.nextLink"].ToString();
-                _logger.LogInformation(nextLink); 
             }
             else 
             {
                 // Use nextLink value to get the page of messages
-                _logger.LogInformation(nextPageLink);
-                var httpRequest = new HttpRequestMessage(HttpMethod.Get, nextPageLink);
-                var response = await _graphServiceClient.HttpProvider.SendAsync(httpRequest);
-                Console.WriteLine(response.StatusCode);
-                var stream = await response.Content.ReadAsStreamAsync();
-                pagedMessages =  _graphServiceClient.HttpProvider.Serializer.DeserializeObject<IUserMessagesCollectionPage>(stream);
-                nextLink = pagedMessages.AdditionalData["@odata.nextLink"].ToString();
-                Console.WriteLine(pagedMessages.Count);
+                UserMessagesCollectionRequest messagesCollectionRequest = 
+                    new UserMessagesCollectionRequest(nextPageLink, _graphServiceClient, null);
+                pagedMessages = await messagesCollectionRequest.GetAsync();
             }
 
-            return (Messages: pagedMessages, NextLink: nextLink);
+            return (Messages: pagedMessages, NextLink: GetNextLink(pagedMessages));
+        }
 
-
-            // var skipValue = pagedMessages
-            //     .NextPageRequest?
-            //     .QueryOptions?
-            //     .FirstOrDefault(
-            //         x => string.Equals("$skip", WebUtility.UrlDecode(x.Name), StringComparison.InvariantCultureIgnoreCase))?
-            //     .Value ?? "0";
-
-            // _logger.LogInformation($"skipValue: {skipValue}");
-
-            //return (Messages: pagedMessages, Skip: int.Parse(skipValue));
+        private string GetNextLink(IUserMessagesCollectionPage pagedMessages) {
+            if (pagedMessages.AdditionalData.TryGetValue("@odata.nextLink", out object value)) {
+                return value.ToString();
+            }
+            return null;
         }
 
     }
 }
+
+// Couldn't get this working to make a "raw" API call due to token not being pass error
+// _logger.LogInformation(nextPageLink);
+// var httpRequest = new HttpRequestMessage(HttpMethod.Get, nextPageLink);
+// var response = await _graphServiceClient.HttpProvider.SendAsync(httpRequest);
+// Console.WriteLine(response.StatusCode);
+// var stream = await response.Content.ReadAsStreamAsync();
+// pagedMessages =  _graphServiceClient.HttpProvider.Serializer.DeserializeObject<IUserMessagesCollectionPage>(stream);
+// nextLink = pagedMessages.AdditionalData["@odata.nextLink"].ToString();
+// Console.WriteLine(pagedMessages.Count);
+
+// var skipValue = pagedMessages
+//     .NextPageRequest?
+//     .QueryOptions?
+//     .FirstOrDefault(
+//         x => string.Equals("$skip", WebUtility.UrlDecode(x.Name), StringComparison.InvariantCultureIgnoreCase))?
+//     .Value ?? "0";
+
+// _logger.LogInformation($"skipValue: {skipValue}");
+
+//return (Messages: pagedMessages, Skip: int.Parse(skipValue));
