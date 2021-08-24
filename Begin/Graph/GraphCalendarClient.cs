359a4b0c5547e5edcb9b3d80d7533c4f4b38b16f
Bernard Bolton <boltonbernard23@gmail.com>
2021-08-23 21:30:12 -0700

    Add start of Begin functionality

--- Begin/Graph/GraphCalendarClient.cs
+++ Begin/Graph/GraphCalendarClient.cs
@@ -14,53 +14,18 @@ namespace DotNetCoreRazor_MSGraph.Graph
 {
     public class GraphCalendarClient
     {
-        private readonly ILogger<GraphCalendarClient> _logger;
-        private readonly GraphServiceClient _graphServiceClient;
+        private readonly ILogger<GraphCalendarClient> _logger = null;
+        private readonly GraphServiceClient _graphServiceClient = null;
 
-        public GraphCalendarClient(ILogger<GraphCalendarClient> logger, GraphServiceClient graphServiceClient)
+        public GraphCalendarClient()
         {
-            _logger = logger;
-            _graphServiceClient = graphServiceClient;
+
         }
 
         public async Task<IEnumerable<Event>> GetEvents(string userTimeZone)
         {
-            _logger.LogInformation($"User timezone: {userTimeZone}");
-            // Configure a calendar view for the current week
-            var startOfWeek = DateTime.Now;
-            var endOfWeek = startOfWeek.AddDays(7);
-
-            var viewOptions = new List<QueryOption>
-            {
-                new QueryOption("startDateTime", startOfWeek.ToString("o")),
-                new QueryOption("endDateTime", endOfWeek.ToString("o"))
-            };
-
-            try
-            {
-                // Use the injected GraphServiceClient object to call Me.CalendarView
-                var calendarEvents = await _graphServiceClient
-                    .Me
-                    .CalendarView
-                    .Request(viewOptions)
-                    .Header("Prefer", $"outlook.timezone=\"{userTimeZone}\"")
-                    .Select(evt => new
-                    {
-                        evt.Subject,
-                        evt.Organizer,
-                        evt.Start,
-                        evt.End
-                    })
-                    .OrderBy("start/DateTime")
-                    .GetAsync();
-
-                return calendarEvents;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogInformation($"Error calling Graph /me/calendaview: {ex.Message}");
-                throw;
-            }
+            // Remove this code
+            return await Task.FromResult<IEnumerable<Event>>(null);
         }
 
         // Used for timezone settings related to calendar
---
+++ Begin/Graph/GraphEmailClient.cs
@@ -0,0 +1,113 @@
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace DotNetCoreRazor_MSGraph.Graph
+{
+    public class GraphEmailClient
+    {
+        private readonly ILogger<GraphEmailClient> _logger;
+        private readonly GraphServiceClient _graphServiceClient;
+
+        public GraphEmailClient(ILogger<GraphEmailClient> logger, GraphServiceClient graphServiceClient)
+        {
+            _logger = logger;
+            _graphServiceClient = graphServiceClient;
+        }
+
+        public async Task<IEnumerable<Message>> GetUserMessages()
+        {
+            try
+            {
+                var emails = await _graphServiceClient.Me.Messages
+                            .Request()
+                            .Select(msg => new
+                            {
+                                msg.Subject,
+                                msg.Body,
+                                msg.BodyPreview,
+                                msg.ReceivedDateTime
+                            })
+                            .OrderBy("receivedDateTime")
+                            .Top(10)
+                            .GetAsync();
+
+                return emails.CurrentPage;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"Error calling Graph /me/messages: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<(IEnumerable<Message> Messages, string NextLink)> GetUserMessagesPage(string nextPageLink = null)
+        {
+            int top = 5;
+            IUserMessagesCollectionPage pagedMessages;
+            
+            if (nextPageLink == null) 
+            {
+                // Get initial page of messages
+                pagedMessages = await _graphServiceClient.Me.Messages
+                        .Request()
+                        .Select(msg => new
+                        {
+                            msg.Subject,
+                            msg.Body,
+                            msg.BodyPreview,
+                            msg.ReceivedDateTime
+                        })
+                        .Top(top)
+                        .OrderBy("receivedDateTime desc")
+                        .GetAsync();
+            }
+            else 
+            {
+                // Use nextLink value to get the page of messages
+                UserMessagesCollectionRequest messagesCollectionRequest = 
+                    new UserMessagesCollectionRequest(nextPageLink, _graphServiceClient, null);
+                pagedMessages = await messagesCollectionRequest.GetAsync();
+            }
+
+            return (Messages: pagedMessages, NextLink: GetNextLink(pagedMessages));
+        }
+
+        private string GetNextLink(IUserMessagesCollectionPage pagedMessages) {
+            if (pagedMessages.AdditionalData.TryGetValue("@odata.nextLink", out object value)) {
+                return value.ToString();
+            }
+            return null;
+        }
+
+    }
+}
+
+// Couldn't get this working to make a "raw" API call due to token not being pass error
+// _logger.LogInformation(nextPageLink);
+// var httpRequest = new HttpRequestMessage(HttpMethod.Get, nextPageLink);
+// var response = await _graphServiceClient.HttpProvider.SendAsync(httpRequest);
+// Console.WriteLine(response.StatusCode);
+// var stream = await response.Content.ReadAsStreamAsync();
+// pagedMessages =  _graphServiceClient.HttpProvider.Serializer.DeserializeObject<IUserMessagesCollectionPage>(stream);
+// nextLink = pagedMessages.AdditionalData["@odata.nextLink"].ToString();
+// Console.WriteLine(pagedMessages.Count);
+
+// var skipValue = pagedMessages
+//     .NextPageRequest?
+//     .QueryOptions?
+//     .FirstOrDefault(
+//         x => string.Equals("$skip", WebUtility.UrlDecode(x.Name), StringComparison.InvariantCultureIgnoreCase))?
+//     .Value ?? "0";
+
+// _logger.LogInformation($"skipValue: {skipValue}");
+
+//return (Messages: pagedMessages, Skip: int.Parse(skipValue));
